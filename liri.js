ecd922221656d3719a668fa9de4dc8cdb81e7f8b
unknown <caylenepicache@gmail.com>
2018-04-06 18:49:15 -0700

    added functions and switchc

--- liri.js
+++ liri.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 var fs = require("fs");
 var request = require('request');
 
-var keys = ('./keys.js');
+var keys = require('./keys.js');
 
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
@@ -11,34 +11,77 @@ var Twitter = require('twitter');
 var client = new Twitter(keys.twitter);
 var spotify = new Spotify(keys.spotify);
 
-var userInput = process.argv[2];
+
+var action = process.argv[2];
+var userInput = process.argv.slice(3).join('+');;
+console.log(action);
 console.log(userInput);
 
+switch(action) {
+    case "my-tweets":
+        myTweetsFx();
+        break;
+    case "spotify-this-song": 
+        spotifyThisSongFx();
+        break;
+    case "movie-this":
+        movieThisFx();
+        break;
+    case "do-what-it-says":
+        doWhatItSaysFx();
+        break;
+}
 
-if (userInput === "spotify-this-song") {
-  if (!process.argv[3]){
-      doSpotify();
-      } else {
-        console.log("lol no")
-}}
+function myTweetsFx() {
+    var params = { screen_name: "LIRI_Bootcamp", count: 20 };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error && response.StatusCode == 200) {
+            console.log(data);
 
-function doSpotify() {
+            console.log("=============================================");
+            console.log("Here are the most recent tweets");
 
-  spotify.search({ type: 'track', query: 'The Sign', limit: 10 }, function(err, data) {
+            for (var i = 0; i < tweets.length; i++) {
+
+                console.log("_____________________________________________");
+                console.log("Tweeted on: " + tweets[i].created_at);
+                console.log(tweets[i].text);
+
+            }
+        }
+    });
+};
+
+function spotifyThisSongFx() {
+    if (userInput === undefined) {
+        userInput = 'The Sign';
+    };
+
+  spotify.search({ type: 'track', query: userInput, limit: 10 }, function(err, data) {
       if (err) {
         return console.log('Error occurred: ' + err);
       }
-      console.log("=============================================");
-      //artist name
-      console.log("The artist name is: " + data.tracks.items[5].artists[0].name)
-      //song name
-      console.log("The song name is: " + data.tracks.items[5].name)
-      //preview link
-      console.log("The preview link is: " + data.tracks.items[5].artists[0].href)
-      //album name
-      console.log("The album name is: " + data.tracks.items[5].album.name)
-      console.log("=============================================");
+      //console.log(data.tracks.items[5]);
+      console.log("-------------------------------------------");
+      console.log("Song Information:")
+      console.log("-------------------------------------------");
+      console.log("");
+      //Song name
+      console.log("Song Name: " + data.tracks.items[5].name)
+      //Artist name
+      console.log("Artist: " + data.tracks.items[5].artists[0].name)
+      //Album name
+      console.log("Album: " + data.tracks.items[5].album.name)
+      //Preview Link
+      console.log("Preview Here: " + data.tracks.items[5].artists[0].href)
+      console.log("");
     });
   
 };
 
+function movieThisFx() {
+    if (userInput === undefined) {
+        userInput = 'Mr.Nobody';
+    };
+    
+}
